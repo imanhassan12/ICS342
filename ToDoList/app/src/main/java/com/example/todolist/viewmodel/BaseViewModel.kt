ec2e78f0f89f944b1706cca59e521c89be553634
Samurai493 <qt4070vo@go.minnstate.edu>
2024-08-14 14:37:38 -0400

    updates

--- ToDoList/app/src/main/java/com/example/todolist/viewmodel/BaseViewModel.kt
+++ ToDoList/app/src/main/java/com/example/todolist/viewmodel/BaseViewModel.kt
@@ -3,5 +3,5 @@ package com.example.todolist.viewmodel
 import androidx.lifecycle.ViewModel
 
 open class BaseViewModel : ViewModel() {
-    protected val apiKey: String = "8927c3c9-bd27-47a5-96cb-e9d3856458bc"
+    protected val apiKey: String = ""
 }
